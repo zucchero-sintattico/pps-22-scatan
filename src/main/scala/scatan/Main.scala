aa3fab5d4a358c1c71a3405270fe1b6d91b229d6
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-08-29 09:59:51 +0200

    Setupped scalafmt

--- src/main/scala/scatan/Main.scala
+++ src/main/scala/scatan/Main.scala
@@ -2,6 +2,13 @@ package scatan
 
 @main
 def main(): Unit =
+  val x = 123421
   println("Hello world!")
+  println("Hello world!")
+  var z = 1
+  z = 2
+  z = 3
+end main
 
-def dummyFunction(): Int = 1
+trait X
+trait Y
