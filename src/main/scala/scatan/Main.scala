72e55f364b90cc33e19c35392b5bc325ca74f3ad
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-08-29 11:08:08 +0200

    Updated scalafmt.conf

--- src/main/scala/scatan/Main.scala
+++ src/main/scala/scatan/Main.scala
@@ -2,13 +2,12 @@ package scatan
 
 @main
 def main(): Unit =
-  val x = 123421
+  val x = 123421 * 1234555 * 12344444
   println("Hello world!")
   println("Hello world!")
   var z = 1
   z = 2
   z = 3
-end main
 
 trait X
 trait Y
