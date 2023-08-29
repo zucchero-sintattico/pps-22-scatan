ba08e81c6e8f13f998e05a2f175eeb4f8cb40559
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-08-29 15:45:38 +0200

    wip: start to implement laminar

--- src/main/scala/scatan/Main.scala
+++ src/main/scala/scatan/Main.scala
@@ -1,15 +1,25 @@
 package scatan
+import com.raquo.laminar.api.L.{*, given}
+import org.scalajs.dom
 
+def appElement(): Element =
+  div(
+    a(
+      href := "https://vitejs.dev",
+      target := "_blank",
+      img(src := "/vite.svg", className := "logo", alt := "Vite logo")
+    ),
+    a(
+      href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+      target := "_blank"
+    ),
+    h1("Hello Laminar!"),
+    div(className := "card", button(tpe := "button")),
+    p(className := "read-the-docs", "Click on the Vite logo to learn more")
+  )
 @main
 def main(): Unit =
-  val x = 123421 * 1234555 * 12344444
-  println("Hello world!")
-  println("Hello world!")
-  var z = 1
-  z = 2
-  z = 3
+  val containerNode = dom.document.querySelector("#root")
 
-
-trait X
-
-trait Y
+  // this is how you render the rootElement in the browser
+  render(containerNode, appElement())
