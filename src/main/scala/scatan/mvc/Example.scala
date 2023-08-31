650edf51c12bfb2f8f896fe6f529716f6801ae70
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-08-31 23:11:07 +0200

    Refactor

--- src/main/scala/scatan/mvc/Example.scala
+++ src/main/scala/scatan/mvc/Example.scala
@@ -2,88 +2,116 @@ package scatan.mvc
 
 trait State
 
-object ModelModule:
-  trait Model[S <: State]:
-    def state: S
-  trait Provider[S <: State]:
-    def model: Model[S]
+package mvc:
+  object Model:
+    class Interface[S <: State](val state: S)
+    trait Provider[S <: State]:
+      def model: Interface[S]
+    def apply[S <: State](state: S): Interface[S] = Interface(state)
 
-object ViewModule:
-  trait Requirements[C <: ControllerModule.Controller[?]] extends ControllerModule.Provider[C]
-  trait View[C <: ControllerModule.Controller[?]](requirements: Requirements[C]):
-    def controller: C = requirements.controller
-    def render(): Unit
-  trait Provider[V <: View[?]]:
-    def view: V
+  object View:
+    trait Requirements[C <: Controller.Interface[?]] extends Controller.Provider[C]
+    trait Interface[C <: Controller.Interface[?]](requirements: Requirements[C]):
+      def controller: C = requirements.controller
+      def show(): Unit
+      def hide(): Unit
+    trait Provider[V <: Interface[?]]:
+      def view: V
+    def apply[V <: Interface[?]](_view: V): Provider[V] = new Provider[V]:
+      override def view: V = _view
 
-object ControllerModule:
-  trait Requirements[V <: ViewModule.View[?]] extends ModelModule.Provider[?] with ViewModule.Provider[V]
-  trait Controller[V <: ViewModule.View[?]](requirements: Requirements[V]):
-    def model: ModelModule.Model[?] = requirements.model
-    def view: V = requirements.view
-  trait Provider[C <: Controller[?]]:
-    def controller: C
+  object Controller:
+    trait Requirements[V <: View.Interface[?]] extends Model.Provider[?] with View.Provider[V]
+    trait Interface[V <: View.Interface[?]](requirements: Requirements[V]):
+      def model: Model.Interface[?] = requirements.model
+      def view: V = requirements.view
+    trait Provider[C <: Interface[?]]:
+      def controller: C
+    def apply[C <: Interface[?]](_controller: C): Provider[C] = new Provider[C]:
+      override def controller: C = _controller
 
-trait MVCProvider[S <: State, C <: ControllerModule.Controller[?], V <: ViewModule.View[?]]
+  trait ApplicationPage[S <: State, C <: Controller.Interface[?], V <: View.Interface[?]](
+      val model: Model.Interface[S],
+      viewFactory: View.Requirements[C] => V,
+      controllerFactory: Controller.Requirements[V] => C
+  ) extends Model.Provider[S]
+      with View.Requirements[C]
+      with Controller.Requirements[V]:
+    override def view: V = viewFactory(this)
+    override def controller: C = controllerFactory(this)
 
-class ModelImpl[S <: State](val state: S) extends ModelModule.Model[S]
+  trait Application[S <: State, Route](val model: Model.Interface[S]):
+    val pages: Map[Route, ApplicationPage[S, ?, ?]]
+
+  trait MementoApplication[Route] extends Application[?, Route]:
+    private var routes: Seq[Route] = Seq.empty
+    def show(route: Route): Unit =
+      routes.lastOption.foreach(pages(_).view.hide())
+      routes = routes :+ route
+      pages(route).view.show()
+    def back(): Unit =
+      routes.lastOption.foreach(pages(_).view.hide())
+      routes = routes.dropRight(1)
+      pages(routes.last).view.show()
+
+//
+// Example
+//
+
+import mvc.*
 case class MyState(value: Int) extends State
 
-trait Application[S <: State, C <: ControllerModule.Controller[?], V <: ViewModule.View[?]](
-    val state: S,
-    viewBuilder: ViewModule.Requirements[C] => V,
-    controllerBuilder: ControllerModule.Requirements[V] => C
-) extends ModelModule.Provider[S]
-    with ViewModule.Requirements[C]
-    with ControllerModule.Requirements[V]:
-  override def model: ModelModule.Model[S] = new ModelImpl(state)
-  override def view: V = viewBuilder(this)
-  override def controller: C = controllerBuilder(this)
+object HomePage:
 
-  def render(): Unit =
-    view.render()
+  def apply(model: Model.Interface[MyState]): ApplicationPage[MyState, HomeController, HomeView] =
+    new ApplicationPage[MyState, HomeController, HomeView](model, new HomeView(_), new HomeController(_)) {}
 
-class HomeView(requirements: ViewModule.Requirements[HomeController])
-    extends ViewModule.View[HomeController](requirements):
-  override def render(): Unit =
-    println("HomeView.render")
-    controller.home()
+  class HomeView(requirements: View.Requirements[HomeController]) extends View.Interface[HomeController](requirements):
+    override def show(): Unit =
+      println("HomeView.show")
+      renderHome()
+    override def hide(): Unit =
+      println("HomeView.hide")
+    def renderHome(): Unit =
+      println("HomeView.renderHome")
+      controller.controlHome()
 
-class HomeController(requirements: ControllerModule.Requirements[HomeView])
-    extends ControllerModule.Controller[HomeView](requirements):
-  def home(): Unit =
-    println("HomeController.home")
-    println(model.state)
+  class HomeController(requirements: Controller.Requirements[HomeView])
+      extends Controller.Interface[HomeView](requirements):
+    def controlHome(): Unit =
+      println("HomeController.controlHome")
 
-class HomeApplication(state: MyState)
-    extends Application[MyState, HomeController, HomeView](
-      state,
-      new HomeView(_),
-      new HomeController(_)
-    )
+object AboutPage:
 
-class GameView(requirements: ViewModule.Requirements[GameController])
-    extends ViewModule.View[GameController](requirements):
-  override def render(): Unit =
-    println("GameView.render")
-    controller.game()
+  def apply(model: Model.Interface[MyState]): ApplicationPage[MyState, AboutController, AboutView] =
+    new ApplicationPage[MyState, AboutController, AboutView](model, new AboutView(_), new AboutController(_)) {}
 
-class GameController(requirements: ControllerModule.Requirements[GameView])
-    extends ControllerModule.Controller[GameView](requirements):
-  def game(): Unit =
-    println("GameController.game")
-    println(model.state)
+  class AboutView(requirements: View.Requirements[AboutController])
+      extends View.Interface[AboutController](requirements):
+    override def show(): Unit =
+      println("AboutView.show")
+      renderAbout()
+    override def hide(): Unit =
+      println("AboutView.hide")
+    def renderAbout(): Unit =
+      println("AboutView.renderAbout")
+      controller.controlAbout()
 
-class GameApplication(state: MyState)
-    extends Application[MyState, GameController, GameView](
-      state,
-      new GameView(_),
-      new GameController(_)
-    )
+  class AboutController(requirements: Controller.Requirements[AboutView])
+      extends Controller.Interface[AboutView](requirements):
+    def controlAbout(): Unit =
+      println("AboutController.controlAbout")
+
+enum Page:
+  case Home, About
+object MyApplication extends Application[MyState, Page](Model(MyState(1))) with MementoApplication[Page]:
+  override val pages: Map[Page, ApplicationPage[MyState, ?, ?]] = Map(
+    Page.Home -> HomePage(this.model),
+    Page.About -> AboutPage(this.model)
+  )
 
 @main def run(): Unit =
-  val state: MyState = MyState(1)
-  var app: Application[?, ?, ?] = HomeApplication(state)
-  app.render()
-  app = GameApplication(state.copy(value = 2))
-  app.render()
+  val app = MyApplication
+  app.show(Page.Home)
+  app.show(Page.About)
+  app.back()
