6f06efacb35d69cebc63788c1b44565953480efd
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-09-08 18:26:26 +0200

    wip(model): add map graph construction

--- src/main/scala/scatan/model/map/GameMap.scala
+++ src/main/scala/scatan/model/map/GameMap.scala
@@ -1,6 +1,26 @@
 package scatan.model.map
 
-class GameMap(layers: Int) extends HexTiledMap:
+import scatan.utils.UnorderedTriple
+import scatan.utils.UnorderedTriple.given
+import scatan.utils.UnorderedPair
+import scatan.model.map.HexTiledMap.*
+
+/** A Spot is unique identified by three hexagons in the map.
+  *
+  * @param hexagons
+  */
+type Spot = UnorderedTriple[Hexagon]
+
+/** A Road connect two spots.
+  */
+type Road = UnorderedPair[Spot]
+
+/** Hexagonal tiled GameMap of Scatan.
+  *
+  * @param layers
+  *   number of concentric circles of hexagons
+  */
+class GameMap(layers: Int) extends HexTiledMap with UndirectedGraph[Spot, Road]:
 
   override def tiles: Set[Hexagon] =
     (for
@@ -9,3 +29,19 @@ class GameMap(layers: Int) extends HexTiledMap:
       s <- -layers to layers
       if r + q + s == 0
     yield Hexagon(r, q, s)).toSet
+
+  override def nodes: Set[Spot] =
+    for
+      hex <- tiles
+      first <- hex.neighbours
+      second <- hex.neighbours
+      if first.isNeighbour(second)
+    yield UnorderedTriple(hex, first, second)
+
+  val spotsPerRoad = 2
+  override def edges: Set[Road] =
+    for
+      first <- nodes
+      second <- nodes
+      if first.toSet.intersect(second.toSet).sizeIs == spotsPerRoad
+    yield UnorderedPair(first, second)
