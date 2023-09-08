c93f243d6deaf4dad1b867f2521e70a4ec108020
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-09-08 10:59:07 +0200

    refactor(graph): generalize dependencies

--- src/main/scala/scatan/model/map/UndirectedGraph.scala
+++ src/main/scala/scatan/model/map/UndirectedGraph.scala
@@ -1,8 +1,10 @@
 package scatan.model.map
 
+import scatan.utils.UnorderedPair
+
 /** A graph is a set of nodes and edges.
   */
-trait UndirectedGraph[Node, Edge <: UnorderedPair[Node, Node]]:
+trait UndirectedGraph[Node, Edge <: UnorderedPair[Node]]:
 
   /** @return
     *   the set of nodes
@@ -13,16 +15,3 @@ trait UndirectedGraph[Node, Edge <: UnorderedPair[Node, Node]]:
     *   the set of edges
     */
   def edges: Set[Edge]
-
-/** An unordered pair of elements.
-  * @param a
-  *   an element
-  * @param b
-  *   another element
-  */
-case class UnorderedPair[A, B](a: A, b: B):
-  override def equals(that: Any): Boolean = that match
-    case UnorderedPair(a1, b1) => (a == a1 && b == b1) || (a == b1 && b == a1)
-    case _                     => false
-
-  override def hashCode: Int = a.hashCode + b.hashCode
