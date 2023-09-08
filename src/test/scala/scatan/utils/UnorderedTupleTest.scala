095a1ef1678b81af681b12e189cc89a1aec86e36
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-09-08 13:34:10 +0200

    fix(unorderedTuple): test for non equality

--- src/test/scala/scatan/utils/UnorderedTupleTest.scala
+++ src/test/scala/scatan/utils/UnorderedTupleTest.scala
@@ -31,9 +31,10 @@ class UnorderedPairTest extends BaseTest with ScalaCheckPropertyChecks:
     }
   }
 
-  it should "be not equals if its elements are different" in {
+  it should "be not equals if, at least, one element is different" in {
+    val itemsInPair = 2
     forAll { (a1: Int, b1: Int, a2: Int, b2: Int) =>
-      whenever(((a1, b1) != (b2, a2)) || a1 != a2 || b1 != b2) {
+      whenever(Set(a1, b1).intersect(Set(a2, b2)).sizeIs < itemsInPair) {
         val pair1 = UnorderedPair(a1, b1)
         val pair2 = UnorderedPair(a2, b2)
         pair1 should not be pair2
@@ -66,9 +67,10 @@ class UnorderedTripleTest extends BaseTest with ScalaCheckPropertyChecks:
     }
   }
 
-  it should "be not equals if its elements are different" in {
+  it should "be not equals if, at least, one element is different" in {
+    val itemsInTriple = 3
     forAll { (a1: Int, b1: Int, c1: Int, a2: Int, b2: Int, c2: Int) =>
-      whenever(a1 != a2 || b1 != b2 || c1 != c2) {
+      whenever(Set(a1, b1, c1).intersect(Set(a2, b2, c2)).sizeIs < itemsInTriple) {
         val triple1 = UnorderedTriple(a1, b1, c1)
         val triple2 = UnorderedTriple(a2, b2, c2)
         triple1 should not be triple2
