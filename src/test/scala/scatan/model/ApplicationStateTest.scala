55158891f96a38f067039e2eb43ae2536f39077d
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-19 19:15:36 +0200

    wip(application-state): add tests

---
+++ src/test/scala/scatan/model/ApplicationStateTest.scala
@@ -0,0 +1,48 @@
+package scatan.model
+
+import scatan.BaseTest
+
+class ApplicationStateTest extends BaseTest:
+
+  "An ApplicationState" should "exists" in {
+    val applicationState: ApplicationState = null
+  }
+
+  it should "not have a game initially" in {
+    val applicationState: ApplicationState = ApplicationState(Option.empty)
+    applicationState.game should be(Option.empty)
+  }
+
+  it should "allow to create a game" in {
+    val applicationState: ApplicationState = ApplicationState(Option.empty)
+    val applicationState2 = applicationState.createGame("Player 1", "Player 2", "Player 3", "Player 4")
+    applicationState2.game should not be (Option.empty)
+  }
+
+  it should "allow to create a game with 3 players" in {
+    val applicationState: ApplicationState = ApplicationState(Option.empty)
+    val applicationState2 = applicationState.createGame("Player 1", "Player 2", "Player 3")
+    applicationState2.game should not be (Option.empty)
+  }
+
+  it should "allow to create a game with 4 players" in {
+    val applicationState: ApplicationState = ApplicationState(Option.empty)
+    val applicationState2 = applicationState.createGame("Player 1", "Player 2", "Player 3", "Player 4")
+    applicationState2.game should not be (Option.empty)
+  }
+
+  it should "not allow to create a game with less than 3 players" in {
+    val applicationState: ApplicationState = ApplicationState(Option.empty)
+    for n <- 0 to 2
+    yield assertThrows[IllegalArgumentException] {
+      applicationState.createGame((1 to n).map(i => s"Player $i")*)
+    }
+  }
+
+  it should "not allow to create a game with more than 4 players" in {
+    val applicationState: ApplicationState = ApplicationState(Option.empty)
+    for n <- 5 to 10
+    yield assertThrows[IllegalArgumentException] {
+      applicationState.createGame((1 to n).map(i => s"Player $i")*)
+    }
+  }
