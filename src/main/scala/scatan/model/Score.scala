5b01cdad6ba57232d8c9ebde1b403d41920fff80
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-22 18:48:45 +0200

    docs: add docs to score file

--- src/main/scala/scatan/model/Score.scala
+++ src/main/scala/scatan/model/Score.scala
@@ -1,7 +1,16 @@
 package scatan.model
 
+/** A Map that contains for each player the number of points they have
+  */
 type Scores = Map[Player, Int]
 
 object Score:
+  /** Returns a map of players to 0 points
+    *
+    * @param players
+    *   the players to create the empty scores map for
+    * @return
+    *   the empty scores map
+    */
   def empty(players: Seq[Player]): Scores =
     players.map(_ -> 0).toMap
