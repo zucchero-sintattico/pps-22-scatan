9bfd45c6fdf0767f7281631922175d34c992c19f
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-09-23 17:09:30 +0200

    refactor(test): add more test to gameWithAwards

--- src/test/scala/scatan/model/GameWithAwards.scala
+++ src/test/scala/scatan/model/GameWithAwards.scala
@@ -10,31 +10,23 @@ class GameWithAwards extends BasicGameTest:
   it should "assign a LongestRoad award if there are conditions" in {
     val game: Game = Game(players = threePlayers)
     val player1 = threePlayers.head
-    val player2 = threePlayers.tail.head
-    val game2 = game.assignBuilding(Building(BuildingType.Road), player1)
-    game2.awards(Award(AwardType.LongestRoad)) should be(Option.empty[(Player, Int)])
-    val game3 = game2.assignBuilding(Building(BuildingType.Road), player1)
-    game3.awards(Award(AwardType.LongestRoad)) should be(Option.empty[(Player, Int)])
-    val game4 = game3.assignBuilding(Building(BuildingType.Road), player1)
-    game4.awards(Award(AwardType.LongestRoad)) should be(Option.empty[(Player, Int)])
-    val game5 = game4.assignBuilding(Building(BuildingType.Road), player1)
-    game5.awards(Award(AwardType.LongestRoad)) should be(Option.empty[(Player, Int)])
-    val game6 = game5.assignBuilding(Building(BuildingType.Road), player1)
-    game6.awards(Award(AwardType.LongestRoad)) should be(Some((player1, 5)))
+    val gameWithAwardReached = game
+      .assignBuilding(Building(BuildingType.Road), player1)
+      .assignBuilding(Building(BuildingType.Road), player1)
+      .assignBuilding(Building(BuildingType.Road), player1)
+      .assignBuilding(Building(BuildingType.Road), player1)
+      .assignBuilding(Building(BuildingType.Road), player1)
+    gameWithAwardReached.awards(Award(AwardType.LongestRoad)) should be(Some((player1, 5)))
   }
 
   it should "assign a LargestArmy award if there are conditions" in {
     val game: Game = Game(players = threePlayers)
     val player1 = threePlayers.head
-    val player2 = threePlayers.tail.head
-    val game2 = game.assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
-    game2.awards(Award(AwardType.LargestArmy)) should be(Option.empty[Player])
-    val game3 = game2.assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
-    game3.awards(Award(AwardType.LargestArmy)) should be(Option.empty[Player])
-    val game4 = game3.assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
-    game4.awards(Award(AwardType.LargestArmy)) should be(Some((player1, 3)))
-    val game5 = game4.assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
-    game5.awards(Award(AwardType.LargestArmy)) should be(Some((player1, 3)))
+    val gameWithAwardReached = game
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+    gameWithAwardReached.awards(Award(AwardType.LargestArmy)) should be(Some((player1, 3)))
   }
 
   it should "not reassign LongestRoad if someone else reach same number of roads " in {
@@ -76,4 +68,37 @@ class GameWithAwards extends BasicGameTest:
       .assignBuilding(Building(BuildingType.Road), player2)
     game2.awards(Award(AwardType.LongestRoad)) should be(Some((player1, 5)))
     game3.awards(Award(AwardType.LongestRoad)) should be(Some((player2, 6)))
+  }
+
+  it should "not reassign LargestArmy if someone else reach same number of knights " in {
+    val game: Game = Game(players = threePlayers)
+    val player1 = threePlayers.head
+    val player2 = threePlayers.tail.head
+    val game2 = game
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+    val game3 = game2
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+    game2.awards(Award(AwardType.LargestArmy)) should be(Some((player1, 3)))
+    game3.awards(Award(AwardType.LargestArmy)) should be(Some((player1, 3)))
+  }
+
+  it should "reassign LargestArmy if someone build more knights then the current award owner" in {
+    val game: Game = Game(players = threePlayers)
+    val player1 = threePlayers.head
+    val player2 = threePlayers.tail.head
+    val game2 = game
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player1, DevelopmentCard(DevelopmentType.Knight))
+    val game3 = game2
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+      .assignDevelopmentCard(player2, DevelopmentCard(DevelopmentType.Knight))
+    game2.awards(Award(AwardType.LargestArmy)) should be(Some((player1, 3)))
+    game3.awards(Award(AwardType.LargestArmy)) should be(Some((player2, 4)))
   }
