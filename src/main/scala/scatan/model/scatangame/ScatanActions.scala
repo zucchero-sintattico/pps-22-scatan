f167bd8ed72d6a8c539be7e0a3d3d4dc149c0635
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-09-24 15:35:45 +0200

    chore: added effects in companion object

--- src/main/scala/scatan/model/scatangame/ScatanActions.scala
+++ src/main/scala/scatan/model/scatangame/ScatanActions.scala
@@ -1,16 +1,28 @@
 package scatan.model.scatangame
 
-import scatan.model.game.{Action, Game}
+import scatan.model.game.Action
 import scatan.model.map.Hexagon
 
 enum ScatanActions(effect: ScatanState => ScatanState = identity) extends Action(effect):
-  case RollDice(result: Int) extends ScatanActions
-  case PlaceRobber(hexagon: Hexagon) extends ScatanActions
-  case StoleCard(player: String) extends ScatanActions
-  case BuildRoad extends ScatanActions
-  case BuildSettlement extends ScatanActions
-  case BuildCity extends ScatanActions
-  case BuyDevelopmentCard extends ScatanActions
-  case PlayDevelopmentCard extends ScatanActions
-  case TradeWithBank extends ScatanActions
-  case TradeWithPlayer extends ScatanActions
+  case RollDice(result: Int) extends ScatanActions(ScatanActions.RollEffect(result))
+  case PlaceRobber(hexagon: Hexagon) extends ScatanActions(ScatanActions.PlaceRobberEffect(hexagon))
+  case StoleCard(player: String) extends ScatanActions(ScatanActions.StoleCardEffect(player))
+  case BuildRoad extends ScatanActions(ScatanActions.BuildRoadEffect)
+  case BuildSettlement extends ScatanActions(ScatanActions.BuildSettlementEffect)
+  case BuildCity extends ScatanActions(ScatanActions.BuildCityEffect)
+  case BuyDevelopmentCard extends ScatanActions(ScatanActions.BuyDevelopmentCardEffect)
+  case PlayDevelopmentCard extends ScatanActions(ScatanActions.PlayDevelopmentCardEffect)
+  case TradeWithBank extends ScatanActions(ScatanActions.TradeWithBankEffect)
+  case TradeWithPlayer extends ScatanActions(ScatanActions.TradeWithPlayerEffect)
+
+object ScatanActions:
+  private def RollEffect(result: Int): ScatanState => ScatanState = ???
+  private def PlaceRobberEffect(hexagon: Hexagon): ScatanState => ScatanState = ???
+  private def StoleCardEffect(player: String): ScatanState => ScatanState = ???
+  private def BuildRoadEffect: ScatanState => ScatanState = ???
+  private def BuildSettlementEffect: ScatanState => ScatanState = ???
+  private def BuildCityEffect: ScatanState => ScatanState = ???
+  private def BuyDevelopmentCardEffect: ScatanState => ScatanState = ???
+  private def PlayDevelopmentCardEffect: ScatanState => ScatanState = ???
+  private def TradeWithBankEffect: ScatanState => ScatanState = ???
+  private def TradeWithPlayerEffect: ScatanState => ScatanState = ???
