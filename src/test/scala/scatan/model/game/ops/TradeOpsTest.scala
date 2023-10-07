121fef35a95b0a50c791148427ec88097d45f952
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-10-07 19:22:10 +0200

    chore:  add more tests

--- src/test/scala/scatan/model/game/ops/TradeOpsTest.scala
+++ src/test/scala/scatan/model/game/ops/TradeOpsTest.scala
@@ -11,23 +11,22 @@ class TradeOpsTest extends BaseScatanStateTest:
 
   "A State with trade Ops" should "allow to trade resources" in {
     val state = ScatanState(threePlayers)
-    val player1 = threePlayers.head
-    val player2 = threePlayers.tail.head
+    val sender = threePlayers.head
+    val receiver = threePlayers.tail.head
     val stateWithResourceAssigned = state
-      .assignResourceCard(player1, ResourceCard(ResourceType.Wood))
-      .flatMap(
-        _.assignResourceCard(player2, ResourceCard(ResourceType.Brick))
-      )
+      .assignResourceCard(sender, ResourceCard(ResourceType.Wood))
+      .flatMap(_.assignResourceCard(sender, ResourceCard(ResourceType.Wood)))
+      .flatMap(_.assignResourceCard(receiver, ResourceCard(ResourceType.Brick)))
     stateWithResourceAssigned match
       case Some(state) =>
-        state.resourceCards(player1) should be(Seq(ResourceCard(ResourceType.Wood)))
-        state.resourceCards(player2) should be(Seq(ResourceCard(ResourceType.Brick)))
         val stateWithTrade =
-          state.tradeWithPlayer(player1, player2, state.resourceCards(player1), state.resourceCards(player2))
+          state.tradeWithPlayer(sender, receiver, state.resourceCards(sender), state.resourceCards(receiver))
         stateWithTrade match
           case Some(state) =>
-            state.resourceCards(player1) should be(Seq(ResourceCard(ResourceType.Brick)))
-            state.resourceCards(player2) should be(Seq(ResourceCard(ResourceType.Wood)))
+            state.resourceCards(sender) should be(Seq(ResourceCard(ResourceType.Brick)))
+            state.resourceCards(receiver) should be(
+              Seq(ResourceCard(ResourceType.Wood), ResourceCard(ResourceType.Wood))
+            )
           case None => fail("Trade not allowed")
       case None => fail("Resources not assigned")
   }
@@ -48,5 +47,28 @@ class TradeOpsTest extends BaseScatanStateTest:
         val stateWithTrade =
           state.tradeWithPlayer(sender, receiver, Seq(ResourceCard(ResourceType.Brick)), state.resourceCards(receiver))
         stateWithTrade should be(None)
+      case None => fail("Resources not assigned")
+  }
+
+  it should "remove only the cards that are involved in the trade from a player " in {
+    val state = ScatanState(threePlayers)
+    val sender = threePlayers.head
+    val receiver = threePlayers.tail.head
+    val stateWithResourceAssigned = state
+      .assignResourceCard(sender, ResourceCard(ResourceType.Wood))
+      .flatMap(_.assignResourceCard(sender, ResourceCard(ResourceType.Wood)))
+      .flatMap(_.assignResourceCard(sender, ResourceCard(ResourceType.Wood)))
+      .flatMap(_.assignResourceCard(receiver, ResourceCard(ResourceType.Brick)))
+    stateWithResourceAssigned match
+      case Some(state) =>
+        val stateWithTrade =
+          state.tradeWithPlayer(sender, receiver, Seq(ResourceCard(ResourceType.Wood)), state.resourceCards(receiver))
+        stateWithTrade match
+          case Some(state) =>
+            state.resourceCards(sender) should be(
+              Seq(ResourceCard(ResourceType.Wood), ResourceCard(ResourceType.Wood), ResourceCard(ResourceType.Brick))
+            )
+            state.resourceCards(receiver) should be(Seq(ResourceCard(ResourceType.Wood)))
+          case None => fail("Trade not allowed")
       case None => fail("Resources not assigned")
   }
