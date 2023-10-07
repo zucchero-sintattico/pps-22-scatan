28f3d1666861603dc26aa4a7664c430bf9048f8f
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-10-07 17:29:56 +0200

    wip(player-card): make the count reactive

--- src/main/scala/scatan/views/game/components/CardsComponent.scala
+++ src/main/scala/scatan/views/game/components/CardsComponent.scala
@@ -3,8 +3,16 @@ package scatan.views.game.components
 import com.raquo.laminar.api.L.*
 import scatan.model.components.ResourceType
 import scatan.model.components.ResourceType.*
+import scatan.model.ApplicationState
+import scatan.model.components.ResourceCard
+import scatan.model.game.config.ScatanPlayer
+import scatan.model.game.ScatanState
 
 object CardsComponent:
+
+  extension (state: ScatanState)
+    def countResourceOf(player: ScatanPlayer)(resourceType: ResourceType): Int =
+      state.resourceCards(player).count(_.resourceType == resourceType)
 
   private val resourcesImg: Map[ResourceType, String] = Map(
     Wood -> "res/img/cards/wood.jpg",
@@ -14,21 +22,27 @@ object CardsComponent:
     Rock -> "res/img/cards/ore.jpg"
   )
 
-  def resourceCardComponent: Element =
+  def resourceCardComponent(using reactiveState: Signal[ApplicationState]): Element =
     div(
       cls := "game-view-card-container",
-      for (resource, path) <- resourcesImg.toList
+      for (resourceType, path) <- resourcesImg.toList
       yield div(
         cls := "game-view-card-item",
         div(
           cls := "game-view-card-count",
-          "0"
+          child.text <-- reactiveState.map(state =>
+            (for
+              game <- state.game
+              currentPlayer = game.turn.player
+              resourceCount = game.state.countResourceOf(currentPlayer)(resourceType)
+            yield resourceCount).getOrElse(0)
+          )
         ),
-        imageComponent(path)
+        cardImageBy(path)
       )
     )
 
-  private def imageComponent(path: String): Element =
+  private def cardImageBy(path: String): Element =
     img(
       cls := "game-view-card",
       src := path
