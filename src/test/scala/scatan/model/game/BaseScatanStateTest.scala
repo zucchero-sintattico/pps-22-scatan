459dedcfb6f9e7fa061e3ff75b44f156857a74fb
Alessandro Mazzoli <alessandro.mazzoli9@studio.unibo.it>
2023-10-07 16:19:04 +0200

    chore: renamed BasicScatanStateTest in BaseScatanStateTest

--- src/test/scala/scatan/model/game/BaseScatanStateTest.scala
+++ src/test/scala/scatan/model/game/BaseScatanStateTest.scala
@@ -5,7 +5,7 @@ import scatan.model.game.config.ScatanPlayer
 import scatan.model.game.ops.EmptySpotsOps.emptySpots
 import scatan.model.map.Spot
 
-abstract class BasicScatanStateTest extends BaseTest:
+abstract class BaseScatanStateTest extends BaseTest:
 
   protected def players(n: Int): Seq[ScatanPlayer] =
     (1 to n).map(i => ScatanPlayer(s"Player $i"))
