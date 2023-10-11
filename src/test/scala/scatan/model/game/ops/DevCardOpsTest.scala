1659c7673fbfea827e2c61726ef7fdaabc4745c2
luigi-borriello00 <luigi.borriello2@studio.unibo.it>
2023-10-11 09:35:22 +0200

    chore: reformat tests

--- src/test/scala/scatan/model/game/ops/DevCardOpsTest.scala
+++ src/test/scala/scatan/model/game/ops/DevCardOpsTest.scala
@@ -2,7 +2,12 @@ package scatan.model.game.ops
 
 import scatan.lib.game.Game
 import scatan.model.components.{DevelopmentCard, DevelopmentType, ResourceCard, ResourceType}
-import scatan.model.game.ops.CardOps.{assignDevelopmentCard, assignResourceCard, buyDevelopmentCard, consumeDevelopmentCard}
+import scatan.model.game.ops.CardOps.{
+  assignDevelopmentCard,
+  assignResourceCard,
+  buyDevelopmentCard,
+  consumeDevelopmentCard
+}
 import scatan.model.game.{BaseScatanStateTest, ScatanState}
 
 class DevCardOpsTest extends BaseScatanStateTest:
