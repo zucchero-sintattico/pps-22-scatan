fd16f5b7a6709b25e9890e1db7d9e73e521edde0
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-10-11 20:18:35 +0200

    wip(random-map): add factory for random content

--- src/main/scala/scatan/model/map/TileContent.scala
+++ src/main/scala/scatan/model/map/TileContent.scala
@@ -15,11 +15,15 @@ trait MapWithTileContent:
     */
   def toContent: Map[Hexagon, TileContent]
 
+trait TileContentConfig:
+  def numbers: Seq[Int]
+  def terrains: Seq[Terrain]
+
 /** A factory to create terrains.
   */
 object TileContentFactory:
 
-  def fixedForLayer2(tiles: Seq[Hexagon]): Map[Hexagon, TileContent] =
+  object ConfigForLayer2 extends TileContentConfig:
     val terrains: List[Terrain] = List(
       4 * Wood,
       4 * Sheep,
@@ -27,16 +31,28 @@ object TileContentFactory:
       3 * Rock,
       3 * Brick
     ).flatten
-
     val numbers =
       2 :: 12 :: (for
         i <- (3 to 11).toList
         if i != 7
       yield List(i, i)).flatten
 
-    val tileContents =
-      terrains.zip(numbers).map(p => TileContent(p._1, Some(p._2)))
-
+  private def fromConfig(using config: TileContentConfig)(tiles: Seq[Hexagon]): Map[Hexagon, TileContent] =
+    val tileContents = config.terrains.zip(config.numbers).map(p => TileContent(p._1, Some(p._2)))
     Map
-      .from(tiles.zip(TileContent(Desert, None) :: tileContents))
+      .from(tiles.zip(TileContent(Desert, None) +: tileContents))
       .withDefaultValue(TileContent(Sea, None))
+
+  def fixedForLayer2(tiles: Seq[Hexagon]): Map[Hexagon, TileContent] =
+    import ConfigForLayer2.*
+    given TileContentConfig = ConfigForLayer2
+    fromConfig(tiles)
+
+  def randomForLayer2(tiles: Seq[Hexagon]): Map[Hexagon, TileContent] =
+    import ConfigForLayer2.*
+    import scala.util.Random.shuffle
+
+    given TileContentConfig with
+      val terrains = shuffle(ConfigForLayer2.terrains)
+      val numbers = shuffle(ConfigForLayer2.numbers)
+    fromConfig(tiles)
