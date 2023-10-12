05725d4801a749cf405aa79881d5a4bdceb18bf3
Manuel Andruccioli <manuel.andruccioli@studio.unibo.it>
2023-10-12 21:22:12 +0200

    wip(map-pick): add map picker in setup view

--- src/main/scala/scatan/controllers/game/SetUpController.scala
+++ src/main/scala/scatan/controllers/game/SetUpController.scala
@@ -3,11 +3,12 @@ package scatan.controllers.game
 import scatan.lib.mvc.{BaseController, Controller}
 import scatan.model.ApplicationState
 import scatan.views.game.SetUpView
+import scatan.model.GameMap
 
 /** This is the controller for the setup page.
   */
 trait SetUpController extends Controller[ApplicationState]:
-  def startGame(usernames: String*): Unit
+  def startGame(gameMap: GameMap, usernames: String*): Unit
 
 object SetUpController:
   def apply(requirements: Controller.Requirements[SetUpView, ApplicationState]): SetUpController =
@@ -21,5 +22,5 @@ private class SetUpControllerImpl(requirements: Controller.Requirements[SetUpVie
     extends BaseController(requirements)
     with SetUpController:
 
-  override def startGame(usernames: String*): Unit =
-    this.model.update(_.createGame(usernames*))
+  override def startGame(gameMap: GameMap, usernames: String*): Unit =
+    this.model.update(_.createGame(gameMap, usernames*))
